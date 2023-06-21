a3a2881b8b6264b35d8ded1d003fc91468813870
sajid2123 <sajidmalhi9@gmail.com>
2023-06-21 12:03:27 +0200

    Variable

---
+++ variables.js
@@ -0,0 +1,16 @@
+var nombre = "Sajid";
+
+//declarar
+var edad;
+//Inicializars
+edad = 20;
+
+
+//Array
+var elementos = ["monitor","telefono"];
+
+//objeto
+var persona = {
+    nombre: "Sajid",
+    edad: 20
+}
