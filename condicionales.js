dcf5ad7fc16c5bd84b65c02a3229558343982624
sajid2123 <sajidmalhi9@gmail.com>
2023-06-21 14:37:45 +0200

    condicionales

---
+++ condicionales.js
@@ -0,0 +1,36 @@
+var edad = 18;
+
+if(edad === 18){
+    console.log("Puedes votar ");
+}else if(edad > 18){
+    console.log("Puedes votar de nuevo");
+}
+else{
+    console.log("Aun no puedes votar");
+}
+
+//Ternario 
+var a = 18;
+a === 18? "Si" : "No";
+
+function fun(jugador,otroJugador){
+    var maquina = otroJugador;
+    var resultado;
+    if(jugador === "piedra" && maquina === "papel") {
+        resultado = "Perdiste"
+    }else if(jugador === "piedra" && maquina === "tijeras") {
+        resultado = "Ganaste"
+    }else if(jugador === "papel" && maquina === "piedra") {
+        resultado = "ganaste"
+    }else if(jugador === "papel" && maquina === "tijeras") {
+        resultado = "Perdiste"
+    }else if(jugador === "tijeras" && maquina === "papel") {
+        resultado = "Ganaste"
+    }else if(jugador === "tijeras" && maquina === "piedra") {
+        resultado = "Perdiste"
+    }else{
+        resultado = "empate";
+    }
+    return resultado;
+}
+fun("piedra","tijeras")
