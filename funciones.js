08ff6ca4e187113f7511774fd7c247d927d41d3a
sajid2123 <sajidmalhi9@gmail.com>
2023-06-21 13:12:04 +0200

    index,variables y funciones

---
+++ funciones.js
@@ -0,0 +1,15 @@
+//Declarativas
+function miFuncion(){
+    return 3;
+}
+//Expresion 
+var miFuncion = function(a,b){
+    return a +b;
+}
+miFuncion();
+miFuncion(1,2);
+
+function saludarEstudiantes(estudiante){
+    console.log(`Hola ${estudiante}`);
+}
+
