d92b5e7f0f9bae0184c41e7332ebcf372af0314c
Sajid ahmad <sajidmalhi9@gmail.com>
2023-07-03 22:57:10 +0200

    Guess the number

---
+++ MasApuntes/Chapter-2-operators.js
@@ -0,0 +1,34 @@
+/**
+ *  ¡¡¡¡OPERATORS!!!!!
+ * 1.Arithmetic operators
+ * -> + Addition 
+ * -> - Subtraction
+ * -> * Multiplication
+ * -> ** Exponentiation
+ * -> / Division
+ * -> % Modulos
+ * -> ++ Increment
+ * -> -- Decrement
+ * 2.assignment Operators
+ * -> = x = y
+ * -> += x = x + y
+ * -> -x x = x - y
+ * -> *= x = x * y
+ * -> /= x = x/y
+ * -> %= x = x % y
+ * -> **= x = x ** y
+ * 3.Comparison operators
+ * -> == equal to
+ * -> != not equal
+ * -> === equal value and type
+ * -> !== not equal value or not equal type
+ * -> > greater than
+ * -> < less than 
+ * -> >= greater than or equal to
+ * -> <= less than or equal to
+ * -> ? trenary operater
+ * 4.Logical operator 
+ * -> && logical and
+ * -> || logical or 
+ * -> ! logical not
+ */
---
+++ MasApuntes/Chapter-3-Loops.js
@@ -0,0 +1,10 @@
+/**
+ * ¡¡¡LOOPS!!!
+ * We use lopps to perform repeated actions.
+ * For example - if you are designed a 
+ * of printing numbers from 1 to 100,it
+ * will be very hectic to do it manually,
+ * Loops help us automate such tasks.
+ * 
+ * 
+ */
