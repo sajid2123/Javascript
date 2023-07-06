f348d66e1fc41b427425208f56c4bfe45132ff8b
Sajid ahmad <sajidmalhi9@gmail.com>
2023-07-06 21:24:17 +0200

    Aprendiendo-react

--- React/travel-list/src/App.js
+++ React/travel-list/src/App.js
@@ -1,3 +1,58 @@
-export default function App(){
-  return <h1>Hello React</h1>
+const initialItems = [
+  {
+    id: 1,
+    description: "Passports",
+    quantity: 2,
+    packed: false
+  },
+  {
+    id: 2,
+    description: "Shirts",
+    quantity: 12,
+    packed: false
+  },
+  {
+    id: 3,
+    description: "Shoes",
+    quantity: 3,
+    packed: false
+  }
+
+]
+
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList itemsObj={initialItems} />
+      <Stats />
+    </div>
+
+  )
+}
+function Logo() {
+  return <h1>Far Away</h1>
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  )
+}
+function PackingList(itemsObj) {
+  return (
+    <div className="list">
+
+    </div>
+  )
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  )
 }
