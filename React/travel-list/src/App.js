b7e15a970e0409bc2e732c5da978576701608efe
sajid2123 <sajidmalhi9@gmail.com>
2023-07-10 13:17:58 +0200

    Aprendiendo Reacy

--- React/travel-list/src/App.js
+++ React/travel-list/src/App.js
@@ -1,58 +1,117 @@
-const initialItems = [
-  {
-    id: 1,
-    description: "Passports",
-    quantity: 2,
-    packed: false
-  },
-  {
-    id: 2,
-    description: "Shirts",
-    quantity: 12,
-    packed: false
-  },
-  {
-    id: 3,
-    description: "Shoes",
-    quantity: 3,
-    packed: false
-  }
-
-]
-
+import React, { useState } from "react";
+import "./index.css";
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList itemsObj={initialItems} />
-      <Stats />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+      />
+      <Stats items={items} />
     </div>
-
-  )
+  );
 }
 function Logo() {
-  return <h1>Far Away</h1>
+  return <h1>Far Away</h1>;
 }
-function Form() {
+function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    setDescription("");
+    setQuantity(1);
+    onAddItems(newItem);
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-    </div>
-  )
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+      />
+      <button>ADD</button>
+    </form>
+  );
 }
-function PackingList(itemsObj) {
+function PackingList({ items, onDeleteItem, onToggleItem }) {
   return (
     <div className="list">
-
+      <ul>
+        {items.map((item) => (
+          <Item
+            item={item}
+            key={item.id}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+          />
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
-function Stats() {
+function Item({ item, onDeleteItem, onToggleItem }) {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
+      <span className={item.packed ? "packed-items" : "null"}>
+        {item.quantity} {item.description}
+      </span>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
+    </li>
+  );
+}
+
+function Stats({ items }) {
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const packedPercentage = Math.round((numPacked * 100) / numItems);
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        You have {numItems} items on your list, and you already packed{" "}
+        {numPacked} ({isNaN(packedPercentage) ? 0 : packedPercentage}%)
+      </em>
     </footer>
-  )
+  );
 }
