4f0742564c794ee0521bb39697d9f2c46249930c
sajid2123 <sajidmalhi9@gmail.com>
2023-07-11 17:16:20 +0200

    Aprendiendo React

--- React/travel-list/src/App.js
+++ React/travel-list/src/App.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 import "./index.css";
+import Logo from "./logo";
+import Form from "./form";
+import PackingList from "./packingList";
+import Stats from "./stats";
 
 export default function App() {
   const [items, setItems] = useState([]);
@@ -17,6 +21,15 @@ export default function App() {
   function handleDeleteItem(id) {
     setItems((items) => items.filter((item) => item.id !== id));
   }
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to clear the list it"
+    );
+
+    if (confirmed) {
+      setItems((e) => []);
+    }
+  }
   return (
     <div className="app">
       <Logo />
@@ -25,116 +38,9 @@ export default function App() {
         items={items}
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
       />
       <Stats items={items} />
     </div>
-  );
-}
-function Logo() {
-  return <h1>Far Away</h1>;
-}
-function Form({ onAddItems }) {
-  const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(1);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    if (!description) return;
-    const newItem = { description, quantity, packed: false, id: Date.now() };
-    setDescription("");
-    setQuantity(1);
-    onAddItems(newItem);
-  }
-  return (
-    <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What do you need for your trip?</h3>
-      <select
-        value={quantity}
-        onChange={(e) => setQuantity(Number(e.target.value))}
-      >
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option value={num} key={num}>
-            {num}
-          </option>
-        ))}
-      </select>
-      <input
-        type="text"
-        placeholder="Item..."
-        value={description}
-        onChange={(e) => {
-          setDescription(e.target.value);
-        }}
-      />
-      <button>ADD</button>
-    </form>
-  );
-}
-function PackingList({ items, onDeleteItem, onToggleItem }) {
-  const [sortBy, setSortBy] = useState("input");
-
-  return (
-    <div className="main">
-      <div className="list">
-        <ul>
-          {items.map((item) => (
-            <Item
-              item={item}
-              key={item.id}
-              onDeleteItem={onDeleteItem}
-              onToggleItem={onToggleItem}
-            />
-          ))}
-        </ul>
-      </div>
-      <div className="actions">
-        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
-          <option value="input">Sort by input</option>
-          <option value="description">Sort by decription</option>
-          <option value="packed">Sort by packed</option>
-        </select>
-        <button>Clear list</button>
-      </div>
-    </div>
-  );
-}
-function Item({ item, onDeleteItem, onToggleItem }) {
-  return (
-    <li>
-      <input
-        type="checkbox"
-        value={item.packed}
-        onChange={() => onToggleItem(item.id)}
-      />
-      <span className={item.packed ? "packed-items" : "null"}>
-        {item.quantity} {item.description}
-      </span>
-      <button onClick={() => onDeleteItem(item.id)}>❌</button>
-    </li>
-  );
-}
-
-function Stats({ items }) {
-  if (!items.length) {
-    return (
-      <footer className="stats">
-        <em>Add items to your List.</em>
-      </footer>
-    );
-  }
-  const numItems = items.length;
-  const numPacked = items.filter((item) => item.packed).length;
-  const packedPercentage = Math.round((numPacked * 100) / numItems);
-  return (
-    <footer className="stats">
-      {packedPercentage === 100 ? (
-        <em>You got everthing, ready to go🛫</em>
-      ) : (
-        <em>
-          You have {numItems} items on your list, and you already packed{" "}
-          {numPacked} ({isNaN(packedPercentage) ? 0 : packedPercentage}%)
-        </em>
-      )}
-    </footer>
   );
 }
